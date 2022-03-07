f1e1adba9a5b170960941efa4d07282e9f5a5d72
Xavier Negron <xaviernegron888@gmail.com>
2022-03-07 17:14:03 -0600

    Create 'Down' indicator

--- contracts/interfaces/IIndicator.sol
+++ contracts/interfaces/IIndicator.sol
@@ -2,10 +2,37 @@
 pragma solidity ^0.8.3;
 
 interface IIndicator {
+
+    struct State {
+        address tradingBot;
+        uint256 value;
+        uint256[] params;
+        uint256[] variables;
+        uint256[] history;
+    }
+
+    /* ========== VIEWS ========== */
+
     /**
     * @dev Returns the name of this indicator.
     */
     function getName() external view returns (string memory);
+
+    /**
+    * @dev Returns the value of this indicator for the given instance.
+    * @param _instance Instance number of this indicator.
+    * @return (uint256[] memory) Indicator value for the given instance.
+    */
+    function getValue(uint256 _instance) external view returns (uint256[] memory);
+
+    /**
+    * @dev Returns the history of this indicator for the given instance.
+    * @param _instance Instance number of this indicator.
+    * @return (uint256[] memory) Indicator value history for the given instance.
+    */
+    function getHistory(uint256 _instance) external view returns (uint256[] memory);
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
 
     /**
     * @dev Creates an instance of this indicator for the contract calling this function.
@@ -23,17 +50,8 @@ interface IIndicator {
     */
     function update(uint256 _instance, uint256 _latestPrice) external;
 
-    /**
-    * @dev Returns the value of this indicator for the given instance.
-    * @param _instance Instance number of this indicator.
-    * @return (uin256) Indicator value for the given instance.
-    */
-    function getValue(uint256 _instance) external view returns (uint256);
+    /* ========== EVENTS ========== */
 
-    /**
-    * @dev Returns the history of this indicator for the given instance.
-    * @param _instance Instance number of this indicator.
-    * @return (uint256[] memory) Indicator value history for the given instance.
-    */
-    function getHistory(uint256 _instance) external view returns (uint256[] memory);
+    event Updated(uint256 indexed instance, uint256 latestPrice, uint256 newValue);
+    event AddedTradingBot(address indexed tradingBot, uint256 instance, uint256[] params);
 }
