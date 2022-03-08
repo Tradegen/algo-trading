ac04bfbd5414276632d2bb57f3d747a5818cf567
Xavier Negron <xaviernegron888@gmail.com>
2022-03-08 13:55:35 -0600

    Add permissions to comparators

--- contracts/interfaces/IComparator.sol
+++ contracts/interfaces/IComparator.sol
@@ -24,13 +24,14 @@ interface IComparator {
     /**
     * @dev Creates an instance of this comparator for the contract calling this function.
     * @notice This function is meant to be called by the TradingBot contract.
+    * @param _tradingBotOwner Address of the trading bot owner.
     * @param _firstIndicatorAddress Address of the comparator's first indicator.
     * @param _secondIndicatorAddress Address of the comparator's second indicator.
     * @param _firstIndicatorInstance Instance number of the first indicator.
     * @param _secondIndicatorInstance Instance number of the second indicator.
     * @return (uint256) Instance number of the comparator.
     */
-    function addTradingBot(address _firstIndicatorAddress, address _secondIndicatorAddress, uint256 _firstIndicatorInstance, uint256 _secondIndicatorInstance) external returns (uint256);
+    function addTradingBot(address _tradingBotOwner, address _firstIndicatorAddress, address _secondIndicatorAddress, uint256 _firstIndicatorInstance, uint256 _secondIndicatorInstance) external returns (uint256);
 
     /**
     * @dev Returns whether the comparator's conditions are met for the given instance.
@@ -40,7 +41,27 @@ interface IComparator {
     */
     function checkConditions(uint256 _instance) external returns (bool);
 
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    /**
+    * @dev Marks this comparator as a default comparator.
+    * @notice This function can only be called by the Components contract.
+    * @notice Once a comparator is marked as default, it cannot go back to being a purchasable comparator.
+    * @notice If a comparator is marked as default, any trading bot can integrate it for free.
+    */
+    function markAsDefault() external;
+
+    /**
+    * @dev Allows the user to use this comparator in trading bots.
+    * @notice This function can only be called by the Components contract.
+    * @notice Meant to be called by the Components contract when a user purchases this comparator.
+    * @param _user Address of the user.
+    */
+    function registerUser(address _user) external;
+
     /* ========== EVENTS ========== */
 
     event AddedTradingBot(address indexed tradingBot, uint256 instance, address firstIndicator, address secondIndicator, uint256 firstIndicatorInstance, uint256 secondIndicatorInstance);
+    event MarkedAsDefault();
+    event RegisteredUser(address user);
 }
