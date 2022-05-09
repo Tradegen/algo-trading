4da89d2fea5b511f3ea79d1176aeb488645b804a
Xavier Negron <xaviernegron888@gmail.com>
2022-05-08 18:45:28 -0700

    Remove circular dependency in TradingBotFactory

--- contracts/TradingBotFactory.sol
+++ contracts/TradingBotFactory.sol
@@ -2,29 +2,30 @@
 
 pragma solidity ^0.8.3;
 
+// OpenZeppelin.
+import "./openzeppelin-solidity/contracts/Ownable.sol";
+
 // Internal references.
 import './TradingBot.sol';
 
 // Inheritance.
 import './interfaces/ITradingBotFactory.sol';
 
-contract TradingBotFactory is ITradingBotFactory {
+contract TradingBotFactory is ITradingBotFactory, Ownable {
     address public immutable componentsRegistry;
     address public immutable candlestickDataFeedRegistry;
-    address public immutable tradingBotRegistry;
+    address public tradingBotRegistry;
     address public immutable keeperRegistry;
     address public immutable tradingBotNFT;
 
-    constructor(address _componentsRegistry, address _candlestickDataFeedRegistry, address _tradingBotRegistry, address _keeperRegistry, address _tradingBotNFT) {
+    constructor(address _componentsRegistry, address _candlestickDataFeedRegistry, address _keeperRegistry, address _tradingBotNFT) Ownable() {
         require(_componentsRegistry != address(0), "TradingBotFactory: Invalid address for _componentsRegistry.");
         require(_candlestickDataFeedRegistry != address(0), "TradingBotFactory: Invalid address for _candlestickDataFeedRegistry.");
-        require(_tradingBotRegistry != address(0), "TradingBotFactory: Invalid address for _tradingBotRegistry.");
         require(_keeperRegistry != address(0), "TradingBotFactory: Invalid address for _keeperRegistry.");
         require(_tradingBotNFT != address(0), "TradingBotFactory: Invalid address for _tradingBotNFT.");
 
         componentsRegistry = _componentsRegistry;
         candlestickDataFeedRegistry = _candlestickDataFeedRegistry;
-        tradingBotRegistry = _tradingBotRegistry;
         keeperRegistry = _keeperRegistry;
         tradingBotNFT = _tradingBotNFT;
     }
@@ -45,6 +46,23 @@ contract TradingBotFactory is ITradingBotFactory {
         return tradingBotContract;
     }
 
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    /**
+    * @notice Sets the address of the TradingBotRegistry contract.
+    * @dev The address is initialized outside of the constructor to avoid a circular dependency with TradingBotRegistry.
+    * @dev This function can only be called by the TradingBotFactory owner.
+    * @dev This function can only be called once.
+    * @param _tradingBotRegistry Address of the TradingBotRegistry contract.
+    */
+    function setTradingBotRegistry(address _tradingBotRegistry) external onlyOwner {
+        require(tradingBotRegistry == address(0), "TradingBotFactory: Already set TradingBotRegistry.");
+
+        tradingBotRegistry = _tradingBotRegistry;
+
+        emit SetTradingBotRegistry(_tradingBotRegistry);
+    }
+
     /* ========== MODIFIERS ========== */
 
     modifier onlyTradingBotRegistry() {
@@ -56,4 +74,5 @@ contract TradingBotFactory is ITradingBotFactory {
     /* ========== EVENTS ========== */
 
     event CreatedTradingBot(address owner, address tradingBotContractAddress);
+    event SetTradingBotRegistry(address tradingBotRegistryAddress);
 }
